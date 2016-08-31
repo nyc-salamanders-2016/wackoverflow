595ebb6bfd526b824e0534380838d578aa077534
Moin Uddin <email": null>
2016-08-31 15:24:39 -0400

    Complete question form and new route

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -4,7 +4,31 @@ get '/questions' do
   erb :'questions/index'
 end
 
-#question details
+#new question form
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+#create new question
+post '/questions' do
+  @question = Question.new(title: params[:question][:title], author: current_user,
+  body: params[:question][:body])
+
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    @errors = user.errors.full_messages
+    erb :'questions/new'
+  end
+
+end
+
+# #question details
+# before '/questions/:id' do
+#   pass if request.path_info.match /new/
+#   # @question = find_and_ensure_entry(params[:id])
+# end
+
 get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
   erb :'questions/details'
@@ -16,13 +40,6 @@ end
 put '/questions/:id' do
 end
 
-#new question form
-get '/questions/new' do
-end
-
-#create new question
-post '/questions' do
-end
 
 #create new answer for a question
 get '/questions/:id/answers/new' do
